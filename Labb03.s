6bbb40781bdbbe58d9926487ec50543fff14b6ab
Emil Hansson <89981140+Hnsson@users.noreply.github.com>
2022-10-12 18:56:12 +0200

    Add files via upload

--- Labb03.s
+++ Labb03.s
@@ -1,5 +1,5 @@
 .data
-  nul:          .byte 0x00
+  null:          .byte 0x00
   end_ptr:      .quad 0
 
   inBuff:    	  .space 64
@@ -17,11 +17,12 @@
     movq stdin, %rdx
     call fgets
     leaq inBuff, %rdi
+    xor %r14, %r14
   inGetLen:
     incq %r14
     incq %rdi
-    movq (%rdi), %r12
-    cmpq $0x0, %r12
+    movb (%rdi), %r12b
+    cmpb %r12b, null
     jne inGetLen
     
     decq %r14
@@ -32,10 +33,22 @@
   getInt:
     call checkInBuffSize
     xor %rsi, %rsi
+    xor %r15, %r15
+
+    movq end_ptr, %rbx
 
     leaq inBuff, %r12
     addq inBuff_ptr, %r12
     jmp getIntLoop
+  getIntAddNegSymbol:
+    movq	$'-',%rdi
+    call	putChar
+    jmp getIntBegining
+  getIntAddSymbol:
+    cmpq $1, %rsi
+    je getIntAddNegSymbol
+    movq	$'+',%rdi
+    call	putChar
   getIntBegining:
     incq %r12
     incq inBuff_ptr
@@ -51,7 +64,8 @@
     cmpb $' ', %dl
     // Concatenate ALL numbers
     //je getIntNotInt
-    // Like Adam
+    // Return after first number
+    //je getIntBegining
     je getIntBegining
     
     cmpb $'0', %dl
@@ -67,7 +81,8 @@
     //
   getIntNeg:
     movq $1, %rsi
-    jmp getIntBegining
+  #  jmp getIntBegining
+    jmp getIntAddSymbol
   getIntMakeNeg:
     negq %r15
     movq $0, %rsi
@@ -92,6 +107,14 @@
     //
 /*-------------- Utmatning --------------*/
   outImage:
+    leaq outBuff, %rsi
+
+    pushq %rsi
+    movb $0x0, %r10b
+    addq outBuff_ptr, %rsi
+    movb %r10b, (%rsi)
+    popq %rsi
+
     movq (outBuff_ptr), %rdx   # message length
     movq $outBuff, %rsi        # message
     movq $1, %rdi              # stdout
@@ -99,25 +122,57 @@
     syscall                    # call kernel
 
     movq $0, outBuff_ptr
-    movq outBuff_ptr, %r14
+    movq outBuff_ptr, %rsi
+    addq outBuff_ptr, %rsi
 
     ret
   putInt:
     // %rdi -> n
-    xor %r14, %r14
-    xor %r15, %r15
-    xor %rsi, %rsi
+    xor %r14, %r14 # Counter
+    xor %r15, %r15 # Maybe not necessary
+    xor %rsi, %rsi # Negative ?
     leaq outBuff, %r15
     addq outBuff_ptr, %r15
+    movq $10, %rcx
+    movq %rdi, %rax
+
 
     cmpq $0, %rdi
     jge putIntLoop
-    movq
-
+    // Number is negative
+    incq %rsi
+    negq %rdi
+    xor %rax, %rax
+    movq %rdi, %rax
   putIntLoop:
+    // Separating the individual numbers
+    xor %rdx, %rdx
+    divq %rcx
+    pushq %rdx
+    incq %r14
+    cmpq $0, %rax # Everything is seperated
+    je putIntCheckSign
+    jmp putIntLoop
 
-
-
+  putIntCheckSign:
+    cmpq $0, %rsi
+    je putIntAssemble
+    movb $'-', (%r15)
+    incq %r15
+    incq outBuff_ptr
+  putIntAssemble:
+    xor %rdx, %rdx
+    cmpq $0, %r14   # Is counter 0?
+    je putIntEnd
+    popq %rdx
+    addb $48, %dl
+    movb %dl, (%r15) # Add char to buffer
+    incq %r15 # Next pos
+    incq outBuff_ptr # Next pos
+    decq %r14 # Go backwards in counter because of stack appliance
+    jmp putIntAssemble
+  putIntEnd:
+    ret
 
   putText:
     leaq outBuff, %r10
@@ -160,10 +215,12 @@
     cmpq $63, outBuff_ptr
     jge outImage
     ret
+  checkBuffSizeImage:
+    call inImage
   checkInBuffSize:
+    cmpq $0, inBuff_ptr
+    jz checkBuffSizeImage
     movq end_ptr, %r10
     cmpq %r10, inBuff_ptr
     jz checkBuffSizeImage
     ret
-  checkBuffSizeImage:
-    call inImage
